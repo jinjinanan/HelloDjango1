c1934bc50e85bddcc4936acc562065adf077f267
陈林波 <jinjinanan@hotmail.com>
2018-09-25 16:56:40 +0800

    自定义amdin站点

--- polls/admin.py
+++ polls/admin.py
@@ -4,5 +4,17 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Question,Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date','question_text']
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+
+admin.site.register(Question,QuestionAdmin)
