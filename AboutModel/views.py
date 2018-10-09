0a855c1b68607ca44987545f726990d79307eadb
陈林波 <jinjinanan@hotmail.com>
2018-10-09 11:10:08 +0800

    上传文件

--- AboutModel/views.py
+++ AboutModel/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 
+from .form import UploadFileForm
+
+from .ProcessFile import handle_uploaded_file
+
 def articles(request,year):
     return HttpResponse('你好,%s'%year)
 
@@ -17,3 +21,14 @@ def edit(request,page_slug):
 
 def year_achive(request,year,foo):
     return HttpResponse('你好, year:%s, foo:%s' % (year,foo))
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST,request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request,'upload.html',{'form':form})
+
