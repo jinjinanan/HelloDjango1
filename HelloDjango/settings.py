d4f84661f576390e59bf20b7432d8c72d7b612be
陈林波 <jinjinanan@hotmail.com>
2018-10-10 13:42:59 +0800

    model

--- HelloDjango/settings.py
+++ HelloDjango/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'app1.apps.App1Config',
     'polls.apps.PollsConfig',
     'AboutModel.apps.AboutmodelConfig',
+    'Practice1.apps.Practice1Config',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +56,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'HelloDjango.urls'
+
 
 TEMPLATES = [
     {
@@ -77,10 +81,26 @@ WSGI_APPLICATION = 'HelloDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# 设置数据库的路由规则方法
+DATABASE_ROUTERS = ['HelloDjango.database_router.DatabaseAppsRouter']
+
+# 设置APP对应的数据库路由表
+DATABASE_APPS_MAPPING = {
+    'Practice1':'Practice1',
+}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'Practice1':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'Practice1',
+        'USER':'root',
+        'PASSWORD':'64231876',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
