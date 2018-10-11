31feb830f84355c2e7b71af501db5dc6597f4cae
陈林波 <jinjinanan@hotmail.com>
2018-10-11 17:59:09 +0800

    登陆验证

--- Practice1/views.py
+++ Practice1/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-import os
+from .models import User
 
 def index(request):
     pass
     return render(request,'Practice1/index.html')
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            username = username.strip()
+            try:
+               user = User.objects.get(name=username)
+               if user.password == password:
+                   return redirect('/Practice1/index/')
+               else:
+                   message = '密码不正确'
+            except:
+               message = '用户名不存在'
+        return render(request,'Practice1/login.html',{'message':message})
     return render(request,'Practice1/login.html')
 
 def register(request):
@@ -15,4 +29,4 @@ def register(request):
 
 def logout(request):
     pass
-    return render(request,'/index/')
+    return render(request,'/Practice1/index/')
